0605b3dcf27e7aa5fb679a4e17a77fdfbd8d891b
navydragon <navydragon@inbox.ru>
2025-01-15 13:35:22 +0300

    upload 15-01

--- constructor_vo_server/settings.py
+++ constructor_vo_server/settings.py
@@ -91,6 +91,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'constructor_vo_server.wsgi.application'
 
 
+DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760
+
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
